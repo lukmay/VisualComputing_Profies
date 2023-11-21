af38a1564d93883237e63708da2b2057690e9309
Stefan Huter <stefan.huter@student.uibk.ac.at>
2023-11-21 14:25:03 +0100

    update project

---
+++ Assignment_3/build/CMakeFiles/assignment_01.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/assignment_1.cpp" "CMakeFiles/assignment_01.dir/src/assignment_1.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/assignment_1.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/math/matrix3d.cpp" "CMakeFiles/assignment_01.dir/src/math/matrix3d.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/math/matrix3d.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/math/matrix4d.cpp" "CMakeFiles/assignment_01.dir/src/math/matrix4d.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/math/matrix4d.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/math/vector2d.cpp" "CMakeFiles/assignment_01.dir/src/math/vector2d.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/math/vector2d.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/math/vector3d.cpp" "CMakeFiles/assignment_01.dir/src/math/vector3d.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/math/vector3d.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/math/vector4d.cpp" "CMakeFiles/assignment_01.dir/src/math/vector4d.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/math/vector4d.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/mygl/base.cpp" "CMakeFiles/assignment_01.dir/src/mygl/base.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/mygl/base.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/mygl/camera.cpp" "CMakeFiles/assignment_01.dir/src/mygl/camera.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/mygl/camera.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/mygl/mesh.cpp" "CMakeFiles/assignment_01.dir/src/mygl/mesh.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/mygl/mesh.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/mygl/shader.cpp" "CMakeFiles/assignment_01.dir/src/mygl/shader.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/mygl/shader.cpp.o.d"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/src/water.cpp" "CMakeFiles/assignment_01.dir/src/water.cpp.o" "gcc" "CMakeFiles/assignment_01.dir/src/water.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/build/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/build/external/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/stefan/Dokumente/Studium_Informatik/Semester_7/PS_Computer_Grafic/Assignment_3/build/external/stb_image/CMakeFiles/stb_image.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
