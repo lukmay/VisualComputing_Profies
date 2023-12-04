6f96917d97cb3bc6a00e719318e174312beb3af9
Deniz Özkaya - csaw1345 <deniz.oezkaya@student.uibk.ac.at>
2023-12-04 11:35:52 +0100

    update, water black?

--- Assignment_4/src/assignment_2.cpp
+++ Assignment_4/src/assignment_2.cpp
@@ -1,261 +1,289 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "mygl/shader.h"
-#include "mygl/model.h"
 #include "mygl/camera.h"
+#include "mygl/model.h"
+#include "mygl/shader.h"
 
 #include "boat.h"
 #include "water.h"
 
-struct
-{
-    Camera camera;
-    bool cameraFollowBoat;
-    float zoomSpeedMultiplier;
+struct Light {
+  Vector3D color;
+  Vector3D position;
+};
 
-    Boat boat;
-    Model water;
+struct {
+  Camera camera;
+  bool cameraFollowBoat;
+  float zoomSpeedMultiplier;
 
-    ShaderProgram shaderBoat;
-    ShaderProgram shaderWater;
+  bool isNight;
 
-    WaterSim waterSim;
+  Boat boat;
+  Model water;
+  Light light;
+
+  ShaderProgram shaderBoat;
+  ShaderProgram shaderWater;
+  ShaderProgram shaderLight;
+
+  WaterSim waterSim;
 } sScene;
 
-struct
-{
-    bool mouseButtonPressed = false;
-    Vector2D mousePressStart;
-    bool keyPressed[Boat::eControl::CONTROL_COUNT] = {false, false, false, false};
+struct {
+  bool mouseButtonPressed = false;
+  Vector2D mousePressStart;
+  bool keyPressed[Boat::eControl::CONTROL_COUNT] = {false, false, false, false};
 } sInput;
 
-
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    /* input for camera control */
-    if(key == GLFW_KEY_0 && action == GLFW_PRESS)
-    {
-        sScene.cameraFollowBoat = false;
-        sScene.camera.lookAt = {0.0f, 0.0f, 0.0f};
-        cameraUpdateOrbit(sScene.camera, {0.0f, 0.0f}, 0.0f);
-    }
-    if(key == GLFW_KEY_1 && action == GLFW_PRESS)
-    {
-        sScene.cameraFollowBoat = false;
-    }
-    if(key == GLFW_KEY_2 && action == GLFW_PRESS)
-    {
-        sScene.cameraFollowBoat = true;
-    }
-
-    /* input for boat control */
-    if(key == GLFW_KEY_W)
-    {
-        sInput.keyPressed[Boat::eControl::THROTTLE_UP] = (action == GLFW_PRESS || action == GLFW_REPEAT);
-    }
-    if(key == GLFW_KEY_S)
-    {
-        sInput.keyPressed[Boat::eControl::THROTTLE_DOWN] = (action == GLFW_PRESS || action == GLFW_REPEAT);
-    }
-
-    if(key == GLFW_KEY_A)
-    {
-        sInput.keyPressed[Boat::eControl::RUDDER_LEFT] = (action == GLFW_PRESS || action == GLFW_REPEAT);
-    }
-    if(key == GLFW_KEY_D)
-    {
-        sInput.keyPressed[Boat::eControl::RUDDER_RIGHT] = (action == GLFW_PRESS || action == GLFW_REPEAT);
-    }
-
-    /* close window on escape */
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    {
-        glfwSetWindowShouldClose(window, true);
-    }
-
-    /* make screenshot and save in work directory */
-    if(key == GLFW_KEY_P && action == GLFW_PRESS)
-    {
-        screenshotToPNG("screenshot.png");
-    }
-}
-
-void mousePosCallback(GLFWwindow* window, double x, double y)
-{
-    if(sInput.mouseButtonPressed)
-    {
-        Vector2D diff = sInput.mousePressStart - Vector2D(x, y);
-        cameraUpdateOrbit(sScene.camera, diff, 0.0f);
-        sInput.mousePressStart = Vector2D(x, y);
-    }
-}
-
-void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
-{
-    if(button == GLFW_MOUSE_BUTTON_LEFT)
-    {
-        sInput.mouseButtonPressed = (action == GLFW_PRESS);
-
-        double x, y;
-        glfwGetCursorPos(window, &x, &y);
-        sInput.mousePressStart = Vector2D(x, y);
-    }
-}
-
-void mouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    cameraUpdateOrbit(sScene.camera, {0, 0}, sScene.zoomSpeedMultiplier * yoffset);
-}
-
-void windowResizeCallback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-    sScene.camera.width = width;
-    sScene.camera.height = height;
-}
-
-void sceneInit(float width, float height)
-{
-    sScene.camera = cameraCreate(width, height, to_radians(45.0), 0.01, 500.0, {40.0, 3.0, 0.0}, {0.0, 0.0, 0.0});
+void keyCallback(GLFWwindow *window, int key, int scancode, int action,
+                 int mods) {
+  /* input for camera control */
+  if (key == GLFW_KEY_0 && action == GLFW_PRESS) {
+    sScene.cameraFollowBoat = false;
+    sScene.camera.lookAt = {0.0f, 0.0f, 0.0f};
+    cameraUpdateOrbit(sScene.camera, {0.0f, 0.0f}, 0.0f);
+  }
+  if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
+    sScene.cameraFollowBoat = false;
+  }
+  if (key == GLFW_KEY_2 && action == GLFW_PRESS) {
     sScene.cameraFollowBoat = true;
-    sScene.zoomSpeedMultiplier = 0.05f;
+  }
 
-    sScene.boat = boatLoad("assets/boat/boat.obj");
-    sScene.water = modelLoad("assets/water/water.obj").front();
+  /* input for boat control */
+  if (key == GLFW_KEY_W) {
+    sInput.keyPressed[Boat::eControl::THROTTLE_UP] =
+        (action == GLFW_PRESS || action == GLFW_REPEAT);
+  }
+  if (key == GLFW_KEY_S) {
+    sInput.keyPressed[Boat::eControl::THROTTLE_DOWN] =
+        (action == GLFW_PRESS || action == GLFW_REPEAT);
+  }
 
-    sScene.shaderBoat = shaderLoad("shader/default.vert", "shader/color.frag");
-    sScene.shaderWater = shaderLoad("shader/water.vert", "shader/color.frag");
+  if (key == GLFW_KEY_A) {
+    sInput.keyPressed[Boat::eControl::RUDDER_LEFT] =
+        (action == GLFW_PRESS || action == GLFW_REPEAT);
+  }
+  if (key == GLFW_KEY_D) {
+    sInput.keyPressed[Boat::eControl::RUDDER_RIGHT] =
+        (action == GLFW_PRESS || action == GLFW_REPEAT);
+  }
+
+  /* close window on escape */
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+
+  /* make screenshot and save in work directory */
+  if (key == GLFW_KEY_P && action == GLFW_PRESS) {
+    screenshotToPNG("screenshot.png");
+  }
+
+  /* control to switch between day and night */
+  if (key == GLFW_KEY_4 && action == GLFW_PRESS) {
+    sScene.isNight = false;
+  }
+  if (key == GLFW_KEY_5 && action == GLFW_PRESS) {
+    sScene.isNight = true;
+  }
 }
 
-void sceneUpdate(float dt)
-{
-    sScene.waterSim.accumTime += dt;
-
-    boatMove(sScene.boat, sScene.waterSim, sInput.keyPressed, dt);
-
-    if (!sScene.cameraFollowBoat)
-        cameraFollow(sScene.camera, sScene.boat.position);
+void mousePosCallback(GLFWwindow *window, double x, double y) {
+  if (sInput.mouseButtonPressed) {
+    Vector2D diff = sInput.mousePressStart - Vector2D(x, y);
+    cameraUpdateOrbit(sScene.camera, diff, 0.0f);
+    sInput.mousePressStart = Vector2D(x, y);
+  }
 }
-void render()
-{
+
+void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods) {
+  if (button == GLFW_MOUSE_BUTTON_LEFT) {
+    sInput.mouseButtonPressed = (action == GLFW_PRESS);
+
+    double x, y;
+    glfwGetCursorPos(window, &x, &y);
+    sInput.mousePressStart = Vector2D(x, y);
+  }
+}
+
+void mouseScrollCallback(GLFWwindow *window, double xoffset, double yoffset) {
+  cameraUpdateOrbit(sScene.camera, {0, 0},
+                    sScene.zoomSpeedMultiplier * yoffset);
+}
+
+void windowResizeCallback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+  sScene.camera.width = width;
+  sScene.camera.height = height;
+}
+
+void sceneInit(float width, float height) {
+  sScene.camera = cameraCreate(width, height, to_radians(45.0), 0.01, 500.0,
+                               {40.0, 3.0, 0.0}, {0.0, 0.0, 0.0});
+  sScene.cameraFollowBoat = true;
+  sScene.zoomSpeedMultiplier = 0.05f;
+  sScene.isNight = false;
+
+  sScene.boat = boatLoad("assets/boat/boat.obj");
+  sScene.water = modelLoad("assets/water/water.obj").front();
+
+  sScene.shaderBoat = shaderLoad("shader/default.vert", "shader/color.frag");
+  sScene.shaderWater = shaderLoad("shader/water.vert", "shader/color.frag");
+
+  sScene.shaderLight = shaderLoad("shader/default.vert", "shader/color.frag");
+}
+
+void sceneUpdate(float dt) {
+  sScene.waterSim.accumTime += dt;
+
+  boatMove(sScene.boat, sScene.waterSim, sInput.keyPressed, dt);
+
+  if (!sScene.cameraFollowBoat)
+    cameraFollow(sScene.camera, sScene.boat.position);
+}
+void render() {
+  /* setup camera and model matrices */
+  Matrix4D proj = cameraProjection(sScene.camera);
+  Matrix4D view = cameraView(sScene.camera);
+  glUseProgram(sScene.shaderBoat.id);
+  shaderUniform(sScene.shaderBoat, "uProj", proj);
+  shaderUniform(sScene.shaderBoat, "uView", view);
+  shaderUniform(sScene.shaderBoat, "uModel", sScene.boat.transformation);
+
+  sScene.isNight ? shaderUniform(sScene.shaderLight, "uLight[0].color",
+                                 Vector3D(0.4f, 0.4f, 0.4f))
+                 : shaderUniform(sScene.shaderLight, "uLight[0].color",
+                                 Vector3D(0.8f, 0.8f, 0.8f));
+
+  shaderUniform(sScene.shaderLight, "uLight[0].position",
+                Vector3D(30.0f, 30.0f, 30.0f));
+  shaderUniform(sScene.shaderLight, "uLight[1].color",
+                Vector3D(1.0f, 1.0f, 1.0f));
+  shaderUniform(sScene.shaderLight, "uLight[1].position",
+                Vector3D(-10.0f, 10.0f, 5.0f));
+  shaderUniform(sScene.shaderLight, "uLight[2].color",
+                Vector3D(1.0f, 1.0f, 1.0f));
+
+  for (unsigned int i = 0; i < sScene.boat.partModel.size(); i++) {
+    auto &model = sScene.boat.partModel[i];
+    glBindVertexArray(model.mesh.vao);
+
+    shaderUniform(sScene.shaderBoat, "uModel", sScene.boat.transformation);
+
+    for (auto &material : model.material) {
+      /* set material properties */
+      shaderUniform(sScene.shaderBoat, "uMaterial.diffuse", material.diffuse);
+      shaderUniform(sScene.shaderBoat, "uMaterial.ambient", material.ambient);
+      shaderUniform(sScene.shaderBoat, "uMaterial.specular", material.specular);
+      shaderUniform(sScene.shaderBoat, "uMaterial.shininess",
+                    material.shininess);
+
+      glDrawElements(
+          GL_TRIANGLES, material.indexCount, GL_UNSIGNED_INT,
+          (const void *)(material.indexOffset * sizeof(unsigned int)));
+    }
+  }
+
+  /* render water */
+  {
+    glUseProgram(sScene.shaderWater.id);
+
     /* setup camera and model matrices */
-    Matrix4D proj = cameraProjection(sScene.camera);
-    Matrix4D view = cameraView(sScene.camera);
-    glUseProgram(sScene.shaderBoat.id);
-    shaderUniform(sScene.shaderBoat, "uProj",  proj);
-    shaderUniform(sScene.shaderBoat, "uView",  view);
-    shaderUniform(sScene.shaderBoat, "uModel",  sScene.boat.transformation);
+    shaderUniform(sScene.shaderWater, "uProj", proj);
+    shaderUniform(sScene.shaderWater, "uView", view);
+    shaderUniform(sScene.shaderWater, "uModel", Matrix4D::identity());
+    /*setup waterSim parameter for water-vertex shader*/
+    shaderUniform(sScene.shaderWater, "u_lastTime", sScene.waterSim.accumTime);
+    shaderUniform(sScene.shaderWater, "u_direction_0",
+                  sScene.waterSim.parameter[0].direction);
+    shaderUniform(sScene.shaderWater, "u_direction_1",
+                  sScene.waterSim.parameter[1].direction);
+    shaderUniform(sScene.shaderWater, "u_direction_2",
+                  sScene.waterSim.parameter[2].direction);
+    /* set material properties */
+    shaderUniform(sScene.shaderWater, "uMaterial.diffuse",
+                  sScene.water.material.front().diffuse);
+    shaderUniform(sScene.shaderWater, "uMaterial.ambient",
+                  sScene.water.material.front().ambient);
+    shaderUniform(sScene.shaderWater, "uMaterial.specular",
+                  sScene.water.material.front().specular);
+    shaderUniform(sScene.shaderWater, "uMaterial.shininess",
+                  sScene.water.material.front().shininess);
 
-    for(unsigned int i = 0; i < sScene.boat.partModel.size(); i++)
-    {
-        auto& model = sScene.boat.partModel[i];
-        glBindVertexArray(model.mesh.vao);
+    glBindVertexArray(sScene.water.mesh.vao);
+    glDrawElements(GL_TRIANGLES, sScene.water.material.front().indexCount,
+                   GL_UNSIGNED_INT,
+                   (const void *)(sScene.water.material.front().indexOffset *
+                                  sizeof(unsigned int)));
+  }
 
-        shaderUniform(sScene.shaderBoat, "uModel", sScene.boat.transformation);
-
-        for(auto& material : model.material)
-        {
-            /* set material properties */
-            shaderUniform(sScene.shaderBoat, "uMaterial.diffuse", material.diffuse);
-            shaderUniform(sScene.shaderBoat, "uMaterial.ambient", material.ambient);
-            shaderUniform(sScene.shaderBoat, "uMaterial.specular", material.specular);
-            shaderUniform(sScene.shaderBoat, "uMaterial.shininess", material.shininess);
-
-            glDrawElements(GL_TRIANGLES, material.indexCount, GL_UNSIGNED_INT, (const void*) (material.indexOffset*sizeof(unsigned int)) );
-        }
-    }
-
-    /* render water */
-    {
-        glUseProgram(sScene.shaderWater.id);
-
-        /* setup camera and model matrices */
-        shaderUniform(sScene.shaderWater, "uProj",  proj);
-        shaderUniform(sScene.shaderWater, "uView",  view);
-        shaderUniform(sScene.shaderWater, "uModel", Matrix4D::identity());
-        /*setup waterSim parameter for water-vertex shader*/
-        shaderUniform(sScene.shaderWater, "u_lastTime", sScene.waterSim.accumTime);
-        shaderUniform(sScene.shaderWater, "u_direction_0", sScene.waterSim.parameter[0].direction);
-        shaderUniform(sScene.shaderWater, "u_direction_1", sScene.waterSim.parameter[1].direction);
-        shaderUniform(sScene.shaderWater, "u_direction_2", sScene.waterSim.parameter[2].direction);
-        /* set material properties */
-        shaderUniform(sScene.shaderWater, "uMaterial.diffuse", sScene.water.material.front().diffuse);
-        shaderUniform(sScene.shaderWater, "uMaterial.ambient", sScene.water.material.front().ambient);
-        shaderUniform(sScene.shaderWater, "uMaterial.specular", sScene.water.material.front().specular);
-        shaderUniform(sScene.shaderWater, "uMaterial.shininess", sScene.water.material.front().shininess);
-
-        glBindVertexArray(sScene.water.mesh.vao);
-        glDrawElements(GL_TRIANGLES, sScene.water.material.front().indexCount, GL_UNSIGNED_INT, (const void*) (sScene.water.material.front().indexOffset*sizeof(unsigned int)) );
-    }
-
-    /* cleanup opengl state */
-    glBindVertexArray(0);
-    glUseProgram(0);
+  /* cleanup opengl state */
+  glBindVertexArray(0);
+  glUseProgram(0);
 }
 
+void sceneDraw() {
+  glClearColor(135.0 / 255, 206.0 / 255, 235.0 / 255, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-void sceneDraw()
-{
-    glClearColor(135.0 / 255, 206.0 / 255, 235.0 / 255, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  /*------------ render scene -------------*/
+  render();
 
-    /*------------ render scene -------------*/
-    render();
-
-    /* cleanup opengl state */
-    glBindVertexArray(0);
-    glUseProgram(0);
+  /* cleanup opengl state */
+  glBindVertexArray(0);
+  glUseProgram(0);
 }
 
-int main(int argc, char** argv)
-{
-    /*---------- init window ------------*/
-    int width = 1280;
-    int height = 720;
-    GLFWwindow* window = windowCreate("Assignment 2 - Shader Programming", width, height);
-    if(!window) { return EXIT_FAILURE; }
+int main(int argc, char **argv) {
+  /*---------- init window ------------*/
+  int width = 1280;
+  int height = 720;
+  GLFWwindow *window =
+      windowCreate("Assignment 2 - Shader Programming", width, height);
+  if (!window) {
+    return EXIT_FAILURE;
+  }
 
-    /* set window callbacks */
-    glfwSetKeyCallback(window, keyCallback);
-    glfwSetCursorPosCallback(window, mousePosCallback);
-    glfwSetMouseButtonCallback(window, mouseButtonCallback);
-    glfwSetScrollCallback(window, mouseScrollCallback);
-    glfwSetFramebufferSizeCallback(window, windowResizeCallback);
+  /* set window callbacks */
+  glfwSetKeyCallback(window, keyCallback);
+  glfwSetCursorPosCallback(window, mousePosCallback);
+  glfwSetMouseButtonCallback(window, mouseButtonCallback);
+  glfwSetScrollCallback(window, mouseScrollCallback);
+  glfwSetFramebufferSizeCallback(window, windowResizeCallback);
 
-    /*---------- init opengl stuff ------------*/
-    glEnable(GL_DEPTH_TEST);
+  /*---------- init opengl stuff ------------*/
+  glEnable(GL_DEPTH_TEST);
 
-    /* setup scene */
-    sceneInit(width, height);
+  /* setup scene */
+  sceneInit(width, height);
 
-    /*-------------- main loop ----------------*/
-    double timeStamp = glfwGetTime();
-    double timeStampNew = 0.0;
-    while(!glfwWindowShouldClose(window))
-    {
-        /* poll and process input and window events */
-        glfwPollEvents();
+  /*-------------- main loop ----------------*/
+  double timeStamp = glfwGetTime();
+  double timeStampNew = 0.0;
+  while (!glfwWindowShouldClose(window)) {
+    /* poll and process input and window events */
+    glfwPollEvents();
 
-        /* update scene */
-        timeStampNew = glfwGetTime();
-        sceneUpdate(timeStampNew - timeStamp);
-        timeStamp = timeStampNew;
+    /* update scene */
+    timeStampNew = glfwGetTime();
+    sceneUpdate(timeStampNew - timeStamp);
+    timeStamp = timeStampNew;
 
-        /* draw all objects in the scene */
-        sceneDraw();
+    /* draw all objects in the scene */
+    sceneDraw();
 
-        /* swap front and back buffer */
-        glfwSwapBuffers(window);
-    }
+    /* swap front and back buffer */
+    glfwSwapBuffers(window);
+  }
 
+  /*-------- cleanup --------*/
+  boatDelete(sScene.boat);
+  modelDelete(sScene.water);
+  shaderDelete(sScene.shaderBoat);
+  shaderDelete(sScene.shaderWater);
+  windowDelete(window);
 
-    /*-------- cleanup --------*/
-    boatDelete(sScene.boat);
-    modelDelete(sScene.water);
-    shaderDelete(sScene.shaderBoat);
-    shaderDelete(sScene.shaderWater);
-    windowDelete(window);
-
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
--- Assignment_4/src/shader/color.frag
+++ Assignment_4/src/shader/color.frag
@@ -21,30 +21,21 @@ in vec3 tViewDir;
 out vec4 FragColor;
 
 uniform Material uMaterial;
-uniform Light uLight;
+uniform Light uLight[4];
 uniform vec3 uViewPos;
 
 void main(void)
 {
-    /* with the globalAmbientColor we can control the day/night setting.
-    (0.8f,0.8f,0.8f) could be daylight
-    (0.4f,0.4f,0.4f) could be night
-    */
     int ambientCoeff = 1;
-    vec3 globalAmbientLightColor = vec3(1.0f,1.0f,1.0f);
-    vec3 ambient = ambientCoeff * uMaterial.diffuse * globalAmbientLightColor;
+    vec3 ambient = ambientCoeff * uMaterial.diffuse * uLight[0].color;
 
     int diffuseCoeff = 1;
-    vec3 lightColor = vec3(1.0f,1.0f,1.0f);
-    vec3 lightDir = normalize(uLight.position - tFragPos);
-    vec3 diffuse = diffuseCoeff * uMaterial.diffuse * lightColor * max(0.0f, dot(normalize(tNormal), lightDir));
+    vec3 diffuse = diffuseCoeff * uMaterial.diffuse * uLight[1].color * max(0.0f, dot(normalize(tNormal), normalize(uLight[0].position - tFragPos)));
 
     int specularCoeff = 1;
     vec3 viewDir = normalize(tViewDir);
-    vec3 lightColor2 = vec3(1.0f,0.0f,0.0f);
-    vec3 halfway = normalize(lightDir + viewDir);
-    vec3 specular = specularCoeff * uMaterial.diffuse * lightColor2 * pow(max(0.0f,dot(normalize(tNormal), halfway)), uMaterial.shininess);
-
+    vec3 halfway = normalize(dot(normalize(tNormal), normalize(uLight[1].position)) + viewDir);
+    vec3 specular = specularCoeff * uMaterial.diffuse * uLight[2].color * pow(max(0.0f, dot(normalize(tNormal), halfway)), uMaterial.shininess);
 
     FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
